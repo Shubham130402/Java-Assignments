1d72a77003029c65adc3465f9aa0010aa8868685
Shubham130402 <shubhamagarwal7219@gmail.com>
2021-09-28 21:25:38 +0530

    Practice codes

--- RECURSION_PROGRAMS/src/com/company/PracticeSet.java
+++ RECURSION_PROGRAMS/src/com/company/PracticeSet.java
@@ -1,10 +1,216 @@
 package com.company;
+import java.util.Arrays;
+
+// Conversion Programs and Bitwise operators
+public class PracticeSet {
+    public static void main(String[] args) {
+        // find XOR of numbers from o to a
+
+    }
+}
+/*
+public class PracticeSet {
+    public static void main(String[] args) {
+        //Given a number n , find the no of set bits in it.
+        int n= 8;
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(setbits(n));
+        /*
+        int n =1000;
+        int p = 0;
+        int N=0;
+        while (n>0){
+            if((n&1)==0){
+                p++;
+
+            }
+            else{
+                N++;
+            }
+            n=n>>1;
+        }
+        System.out.println(N);
+        System.out.println(p);
+
+         // end comment
+    }
+    public static int setbits(int n){
+        int count = 0;
+        while(n>0){
+            count++;
+            n = (n& (n-1));
+        }
+        return count;
+    }
+}
+*/
+/*
+public class PracticeSet {
+    public static void main(String[] args) {
+        // output of the any of the power of the base
+        int power = 0;
+        int ans = 1;
+        int base = -3;
+
+        while(power>0){
+            if((power&1) ==1){
+                ans = ans*base;
+            }
+            base *= base;
+            power = power >>1;
+        }
+        System.out.println(ans);
+
+
+    }
+}
+
+ */
+/*
+public class PracticeSet {
+    public static void main(String[] args) {
+        // find the sum of the nth row in pascal triangle
+        int j = 5; // row number
+        System.out.println(Math.pow(2,j-1));
+
+        // you are given a number if it is pwer of 2 or not
+        int i = 16;
+        boolean ans;
+        for (int n = 0; n <= i; n++) {
+            ans = ((n & (n - 1)) == 0);
+            System.out.println(ans);
+        }
+    }
+
+    }
+
+ */
+
+/*
+public class PracticeSet {
+    public static void main(String[] args) {
+        // Msgic Number 5 ki powers
+        int n =4;
+        int ans = 0;
+        int base =5;
+        while(n>0){
+            int last = n&1;
+            n = n>>1;
+            ans += last*base ;
+            base = base*5;
+
+        }
+        System.out.println(ans);
+    }
+}
+
+ */
+/*
+//binary to decimal conversion program
+public class PracticeSet {
+    public static void main(String[] args) {
+        int binary = 11110110;
+        System.out.println(getdecimal(binary));
+    }
+    public static int getdecimal(int n) {
+        int decimal =0;
+        int p =0 ;
+        while (true) {
+            if (n == 0) {
+                break;
+            } else {
+                int temp = n%10;
+                decimal+= temp*Math.pow(2,p);
+                n=n/10;
+                p++;
+            }
+        }
+        return decimal;
+    }
+}
+
+ */
+/*
 
 public class PracticeSet {
     public static void main(String[] args) {
-        
+        int[] arr = {2, 3, 4, 6, 2, 3, 4};
+        int decNum = 10;
+
+        //decimal to binary conversion
+        StringBuilder sb = new StringBuilder();
+        while(decNum !=0){
+            int num = decNum%2;
+            sb.append(num);
+            decNum = decNum/2;
+        }
+        System.out.println("the Output " + sb);
+        System.out.println("the binary conversion of is : "+ sb.reverse());
+        System.out.println(sb.toString());
+        System.out.println(Arrays.toString(sb.codePoints().toArray()));
+        //binary to decimal conversion
+        String  binary = "10101010";
+        int decimal = Integer.parseInt(binary,2);
+        System.out.println(decimal);
+
+
     }
 }
+
+ */
+// palindrome
+/*
+public class PracticeSet {
+    public static void main(String[] args) {
+        int x = -121;
+        System.out.println(isPalindrome(x));
+    }
+    public static boolean isPalindrome(int x) {
+        int rev = 0;
+        int num =x;
+        while (num!=0){
+            int y= rev*10 + num%10;
+            if((y-num%10)/10 != rev){
+                return false;
+            }
+            else{
+                rev = y;
+            }
+            num= num/10;
+        }
+        if(x == rev){
+            return true;
+        }
+        return false;
+    }
+    }
+
+ */
+/*
+
+public class PracticeSet {
+    public static void main(String[] args) {
+        int x= -0;
+        System.out.println(reverse(x));
+    }
+    public static int reverse(int x){
+        int rev = 0;
+        while (x!=0){
+            int y = rev*10 + x%10;
+            if((y-x%10)/10 != rev){
+                return 0;
+            }
+            else{
+                rev = y;
+            }
+            x= x/10;
+        }
+        return rev;
+    }
+
+}
+
+ */
 /*
 // sum a series of numbers
 // example n=5
